b1cc778f5d91e56958259e9f7acf6d3ce9f3c009
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2024-01-19 09:56:38 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+tem] [MY-010232] [Server] XA crash recovery finished.
+2023-07-26T10:14:25.544949Z 0 [Warning] [MY-010068] [Server] CA certificate ca.pem is self signed.
+2023-07-26T10:14:25.544950Z 0 [System] [MY-013602] [Server] Channel mysql_main configured to support TLS. Encrypted connections are now supported for this channel.
+2023-07-26T10:14:25.584951Z 0 [System] [MY-010931] [Server] C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld.exe: ready for connections. Version: '8.0.23'  socket: ''  port: 3306  MySQL Community Server - GPL.
+2023-07-26T10:46:40.058267Z 0 [System] [MY-013105] [Server] C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld.exe: Normal shutdown.
+2023-07-26T10:46:45.721077Z 0 [System] [MY-010910] [Server] C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld.exe: Shutdown complete (mysqld 8.0.23)  MySQL Community Server - GPL.
+2023-07-26T10:48:25.597911Z 0 [Warning] [MY-010915] [Server] 'NO_ZERO_DATE', 'NO_ZERO_IN_DATE' and 'ERROR_FOR_DIVISION_BY_ZERO' sql modes should be used with strict mode. They will be merged with strict mode in a future release.
+2023-07-26T10:48:25.597913Z 0 [System] [MY-010116] [Server] C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld.exe (mysqld 8.0.23) starting as process 2080
+2023-07-26T10:48:26.596313Z 1 [System] [MY-013576] [InnoDB] InnoDB initialization has started.
+2023-07-26T10:48:35.020328Z 1 [System] [MY-013577] [InnoDB] InnoDB initialization has ended.
+2023-07-26T10:48:35.550730Z 0 [System] [MY-011323] [Server] X Plugin ready for connections. Bind-address: '::' port: 33060
+2023-07-26T10:48:37.032731Z 0 [Warning] [MY-010068] [Server] CA certificate ca.pem is self signed.
+2023-07-26T10:48:37.032732Z 0 [System] [MY-013602] [Server] Channel mysql_main configured to support TLS. Encrypted connections are now supported for this channel.
+2023-07-26T10:48:37.204332Z 0 [System] [MY-010931] [Server] C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld.exe: ready for connections. Version: '8.0.23'  socket: ''  port: 3306  MySQL Community Server - GPL.
+2023-07-26T12:10:18.207405Z 0 [
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        private int mark1;
+        public int Mark1
+        {
+            get { return mark1 ; }
+            set { mark1 = value; }
+        }
+
+        private int mark2 ;
+        public int Mark2
+        {
+            get { return mark1; }
+            set { mark2 = value; }
+        }
+        private int mark3;
+        public int Mark3
+        {
+            get { return mark3; }
+            set { mark3 = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+       // public int Id;
+       public Student(int mark1,int mark2,int mark3)
+       {
+            this.mark1 = mark1;
+            this.mark2 = mark2;
+            this.mark3 = mark3;
+            
+       }
+        //public Student(int mark1,int mark2,int mark3) 
+        //{
+            
+        //}
+
+
+        private bool RangeMark(int mark)
+        {
+            return mark > 0 || mark < 5;
+        }
+
+        private int Avergage()
+        {
+            return (mark1 + mark2 + mark3) / 3;
+        }
+
+        private object GetMark()
+        {
+             object p = Avergage();
+            if ((bool)(p = true))
+            {
+                if (RangeMark(Mark1) && RangeMark(Mark2) && RangeMark(Mark3))
+                {
+                    if (mark1 >= 2)
+                    {
+                        Console.WriteLine("Двоечники");
+                    }
+                    else
+
+                    if (mark2 == 3)
+                    {
+                        Console.WriteLine("Троечники");
+                    }
+                    else
+                        if (mark3 == 4)
+                    {
+                        Console.WriteLine("Хорошисты");
+                    }
+                    else { Console.WriteLine("Отличники"); }
+
+                    
+                }
+                
+            }
+            return p;
+        }
+
+        public void GetStudent()
+        {
+          Console.WriteLine($"Его оценки-{mark1},{mark2},{mark3}\n{GetMark()}");
+        }
+
+
+    }
+}
